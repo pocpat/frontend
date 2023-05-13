87b0394568e47faeebacbbaa6405d02fd2287235
Elena Kroupkin <pocoat@gmail.com>
2023-05-13 13:59:25 +1200

    init commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Insurance API - React App will be here :)
         </a>
       </header>
     </div>
